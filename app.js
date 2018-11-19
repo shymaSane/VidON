bd1547783a374d15570dd3be5e627808e3da0cc8
shymaSane <shymaSane1@gmail.com>
2018-11-19 18:19:29 +0200

    basic middleware

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require ('express');
 const app = express();
 const port = 5000;
 
+//using middleware:
+
+app.use((req, res, next) => {
+    req.name = "shyma al sane";
+    next();
+})
+
 //Index Route
-//when ever we add sth new we need to restart the server
+//when ever we add sth new we need to restart the server>>> so you have to install nodemon and it ll restat automatically 
 app.get('/', (req, res) => {
+    console.log(req.name)
     res.send('Index!!!!')
-    
 })
 
 //About Route
