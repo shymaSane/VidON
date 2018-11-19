43c30863ced9816c89f2466200abf2a6aef4d9f0
shymaSane <shymaSane1@gmail.com>
2018-11-19 17:53:16 +0200

    basic routing & nodemon

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require ('express');
 const app = express();
-const port = 5000
+const port = 5000;
+
+//Index Route
+//when ever we add sth new we need to restart the server
+app.get('/', (req, res) => {
+    res.send('Index!!!!')
+    
+})
+
+//About Route
+app.get('/about', (req, res) => {
+    res.send('ABOUT!!!!')
+})
 
 app.listen(port, () => {
     console.log(`starting on port number ${port}`)
