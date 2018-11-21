b1dcd2bcb315f6b23c3c298c9cbc8501a7ca0294
shymaSane <shymaSane1@gmail.com>
2018-11-21 19:59:19 +0200

    connect mongoose with mongoDB

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ const express = require ('express');
 const app = express();
 const exphbs = require('express-handlebars')
 const port = 5000;
+const mongoose = require('mongoose');
+
+//connect mongoose to mongodb
+const mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB)
+
+//use the global promise library
+mongoose.Promise = global.Promise;
+
+//get default connection
+const db = mongoose.connection
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 
 //add handlebars-express middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//Create, Read, Update and Delete (CRUD) operations
 //Home
 app.get('/', (req, res) => {
     //we can add dynmaic variables to handlebars
