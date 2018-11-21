6159479f01dc7fa85c9755c586d6e9420eadf37b
shymaSane <shymaSane1@gmail.com>
2018-11-21 20:16:21 +0200

    basic schema

--- app.js
+++ app.js
@@ -17,7 +17,15 @@ const db = mongoose.connection
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+//making schema
+const Schema = mongoose.Schema
+const ideas = new Schema ({
+    title: String,
+    idea: String
+})
 
+//compile model for schema
+const IdeasModel = mongoose.model('IdeasModel', ideas)
 
 //add handlebars-express middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
