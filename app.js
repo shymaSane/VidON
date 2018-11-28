0764cc6c2005f83da8e5c67c735fffc4536f9d49
shymaSane <shymaSane1@gmail.com>
2018-11-28 19:10:53 +0200

    separates routers using express routers

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require ('express');
-const exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars');
 const port = 5000;
 const mongoose = require('mongoose');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
 const flash = require('connect-flash');
 //body parser for the from
-const bodyParser = require('body-parser')
-const session = require('express-session')
-
+const bodyParser = require('body-parser');
+const session = require('express-session');
+//passpost setup
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
 
 //entry file 
 const app = express();
+
+//load routes:
+const ideas = require('./routes/ideas')
+const users = require('./routes/users')
+
 
 //middleware for flash messaging 
 app.use(session({
@@ -44,9 +51,7 @@ mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
 //use the global promise library or you ll get deprecation warning mpromise
 mongoose.Promise = global.Promise;
 
-//load Ideas model:
-require('./models/Ideas')
-const Idea = mongoose.model('Ideas')
+
 
 
 //add handlebars-express middleware
@@ -54,6 +59,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 //Create, Read, Update and Delete (CRUD) operations
+
 //Home
 app.get('/', (req, res) => {
     //we can add dynmaic variables to handlebars
@@ -65,90 +71,8 @@ app.get('/about', (req, res) => {
     res.render('about')
 })
 
-//add ideas
-app.get('/ideas/add', (req, res) => {
-    res.render('add')
-})
-
-//fetch ideas from mogodb and show them
-app.get('/ideas', (req,res) => {
-    Idea.find({})
-    .then(ideas => {
-        res.render('ideas',{
-            ideas
-        })
-    })
-})
-
-//grab idea to edit with certain id
-app.get('/ideas/edit/:id', (req, res) => {
-    const query = req.params.id;
-    console.log(query)
-    //find returns array with the results in it
-    Idea.findOne({_id: query})
-    .then(idea => {
-        console.log(idea)
-        res.render('edit', {
-            idea
-        })
-    })
-    
-})
-
-
-//post form request(note form wont pst if it was empty so we need t add validation):
-app.post('/ideas', (req,res) =>{
-    let errors = []
-    let title = req.body.title
-    let detailes = req.body.detailes
-    if(!req.body.title){
-        errors.push('Title is missing! please enter your title.')
-    }
-    if(!req.body.detailes){
-        errors.push('idea is missing! please enter your idea.')
-    }
-    //handle the form
-    if(errors.length > 0){
-        res.render('add', {
-            //note we get name of fieldes from name attribute in add.handlebras
-            errors,
-            title,
-            detailes
-        })
-    } else {
-        //create instance from model Idea
-        const newIdea = new Idea({
-           title,
-           detailes 
-        })
-        newIdea.save()
-        .then(() => {
-            req.flash('success_msg', 'Video Idea has been added successfuly')
-            res.redirect('/ideas')
-        })
-        
-    }
-
-})
-
-//form put request 
-
-app.put('/ideas/:id', (req, res) => {
-    Idea.update({_id: req.params.id}, req.body)
-    .then(() => {
-        req.flash('success_msg', 'Video Idea has been updated successfuly')
-        res.redirect('/ideas') 
-    })
-})
-
-//delete idea 
-app.delete('/ideas/:id', (req, res) => {
-    Idea.remove({_id: req.params.id})
-    .then( () => {
-        req.flash('success_msg', 'Video Idea has been deleted successfuly')
-        res.redirect('/ideas')
-    })
-})
+app.use('/ideas', ideas)
+app.use('/users', users)
 
 app.listen(port, () => {
     console.log(`starting on port number ${port}`)
---
+++ routes/ideas.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+//load Ideas model:
+require('../models/Ideas')
+const Idea = mongoose.model('Ideas')
+
+//fetch ideas from mogodb and show them
+router.get('/', (req,res) => {
+    Idea.find({})
+    .then(ideas => {
+        res.render('ideas',{
+            ideas
+        })
+    })
+})
+
+//add ideas
+router.get('/add', (req, res) => {
+    res.render('add')
+})
+
+//grab idea to edit with certain id
+router.get('/edit/:id', (req, res) => {
+    const query = req.params.id;
+    console.log(query)
+    //find returns array with the results in it
+    Idea.findOne({_id: query})
+    .then(idea => {
+        console.log(idea)
+        res.render('edit', {
+            idea
+        })
+    })
+    
+})
+
+//post form request(note form wont pst if it was empty so we need t add validation):
+router.post('/', (req,res) =>{
+    let errors = []
+    let title = req.body.title
+    let detailes = req.body.detailes
+    if(!req.body.title){
+        errors.push('Title is missing! please enter your title.')
+    }
+    if(!req.body.detailes){
+        errors.push('idea is missing! please enter your idea.')
+    }
+    //handle the form
+    if(errors.length > 0){
+        res.render('add', {
+            //note we get name of fieldes from name attribute in add.handlebras
+            errors,
+            title,
+            detailes
+        })
+    } else {
+        //create instance from model Idea
+        const newIdea = new Idea({
+           title,
+           detailes 
+        })
+        newIdea.save()
+        .then(() => {
+            req.flash('success_msg', 'Video Idea has been added successfuly')
+            res.redirect('/ideas')
+        })
+        
+    }
+
+})
+
+//form put request 
+
+router.put('/:id', (req, res) => {
+    Idea.update({_id: req.params.id}, req.body)
+    .then(() => {
+        req.flash('success_msg', 'Video Idea has been updated successfuly')
+        res.redirect('/ideas') 
+    })
+})
+
+//delete idea 
+router.delete('/:id', (req, res) => {
+    Idea.remove({_id: req.params.id})
+    .then( () => {
+        req.flash('success_msg', 'Video Idea has been deleted successfuly')
+        res.redirect('/ideas')
+    })
+})
+
+module.exports = router
