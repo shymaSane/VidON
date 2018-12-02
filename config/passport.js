d58dcf44d94b4694286b6f2b643274bd571e4acb
shymaSane <shymaSane1@gmail.com>
2018-12-02 14:19:16 +0200

    refuses login when email does not match

--- config/passport.js
+++ config/passport.js
@@ -8,6 +8,13 @@ const User = mongoose.model('users')
 module.exports = function (passport){
     //usernameFiled we specify it because we dont use user name
     passport.use(new LocalStrategy({usernameField: 'email'},(email, password, done) => {
-
+        
+        User.findOne({email})
+        .then(user => {
+            if(!user){
+                console.log(email)
+                return done(null, false, {message: "No User Found"})
+            }
+        })
     }))
 }
